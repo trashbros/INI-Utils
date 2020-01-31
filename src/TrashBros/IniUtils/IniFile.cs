b9a0569eb48efc21db65f63e3edc4c09e4c0614d
Blink The Things <blinkthethings@gmail.com>
2020-01-31 13:48:02 -0500

    Initial commit

---
+++ src/TrashBros/IniUtils/IniFile.cs
@@ -0,0 +1,92 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+
+namespace TrashBros.IniUtils
+{
+    /// <summary>
+    /// Represents an INI file that settings can be written and read from.
+    /// </summary>
+    public class IniFile
+    {
+        #region Private Fields
+
+        /// <summary>
+        /// The file name of the INI file.
+        /// </summary>
+        private readonly string _fileName;
+
+        #endregion Private Fields
+
+        #region Internal Classes
+
+        /// <summary>
+        /// Class containing all P/Invokes
+        /// </summary>
+        internal static class NativeMethods
+        {
+            #region Internal Methods
+
+            [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
+            internal static extern uint GetPrivateProfileString(string lpAppName, string lpKeyName, string lpDefault, StringBuilder lpReturnedString, int nSize, string lpFileName);
+
+            [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
+            internal static extern bool WritePrivateProfileString(string lpAppName, string lpKeyName, string lpString, string lpFileName);
+
+            #endregion Internal Methods
+        }
+
+        #endregion Internal Classes
+
+        #region Public Fields
+
+        /// <summary>
+        /// The maximum value size in characters that can be read.
+        /// </summary>
+        public const int MaxValueSize = 1024;
+
+        #endregion Public Fields
+
+        #region Public Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IniFile"/> class using the specified INI
+        /// file name.
+        /// </summary>
+        /// <param name="fileName">Name of the INI file.</param>
+        public IniFile(string fileName)
+        {
+            _fileName = fileName;
+        }
+
+        #endregion Public Constructors
+
+        #region Public Methods
+
+        /// <summary>
+        /// Gets the value of the specified key in the specified section.
+        /// </summary>
+        /// <param name="section">The section.</param>
+        /// <param name="key">The key.</param>
+        /// <param name="defaultValue">The default value.</param>
+        /// <returns>The value.</returns>
+        public string Get(string section, string key, string defaultValue = "")
+        {
+            StringBuilder sb = new StringBuilder(MaxValueSize);
+            _ = NativeMethods.GetPrivateProfileString(section, key, defaultValue, sb, sb.Capacity, _fileName);
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Sets the value of the specified key in the specified section.
+        /// </summary>
+        /// <param name="section">The section.</param>
+        /// <param name="key">The key.</param>
+        /// <param name="value">The value.</param>
+        public void Set(string section, string key, string value)
+        {
+            NativeMethods.WritePrivateProfileString(section, key, value, _fileName);
+        }
+
+        #endregion Public Methods
+    }
+}
---
+++ src/TrashBros/IniUtilsTest/IniFileTest.cs
@@ -0,0 +1,54 @@
+using Xunit;
+using TrashBros.IniUtils;
+using System.IO;
+
+namespace IniUtilsTest
+{
+    public class IniFileTest
+    {
+        [Fact]
+        public void SettingAValueCreatesANewFile()
+        {
+            // Create a new temporary file to get a temp file name and delete it
+            string fileName = Path.GetTempFileName();
+            File.Delete(fileName);
+
+            // Verify that the file isn't there
+            Assert.False(File.Exists(fileName));
+
+            // Create a new IniFile with the temp file name
+            var iniFile = new IniFile(fileName);
+
+            // Set a value
+            iniFile.Set("global", "color", "purple");
+
+            // Verify that the file was created
+            Assert.True(File.Exists(fileName));
+
+            // Clean up after ourselves
+            File.Delete(fileName);
+        }
+
+        [Fact]
+        public void CanGetAValue()
+        {
+            // Create a simple ini file with one value
+            string fileName = Path.GetTempFileName();
+            string[] lines = { "[global]", "color=purple" };
+            File.WriteAllLines(fileName, lines);
+
+            // Create a new IniFile with the temp file name
+            var iniFile = new IniFile(fileName);
+
+            // Get the value
+            string color = iniFile.Get("global", "color");
+
+            // Verify that the file was created
+            Assert.Equal("purple", color);
+
+            // Clean up after ourselves
+            File.Delete(fileName);
+        }
+
+    }
+}
